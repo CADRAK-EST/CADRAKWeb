6e59c49fe69d1962a6d161f8f5976b61d8a79f6c
Lauri Sarap <lauri.aardla@gmail.com>
2024-07-01 22:59:52 +0300

    Files page now allows the switching of files

--- src/components/FilesPage/FilesPage.js
+++ src/components/FilesPage/FilesPage.js
@@ -1,9 +1,28 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { setSelectedFile } from '../../slices/fileSlice';
+import { clearParsedData, setParsedData } from '../../slices/parsedDataSlice';
+import { clearPage, setReportCard } from '../../slices/pageDataSlice';
+import useParsedData from '../../hooks/UseParsedData';
 import './FilesPage.css';
 
 const FilesPage = () => {
   const files = useSelector((state) => state.file.files);
+  const dispatch = useDispatch();
+  const fetchParsedData = useParsedData();
+
+  const handleFileClick = (file) => {
+    dispatch(setSelectedFile(file));
+    dispatch(clearParsedData());
+    dispatch(clearPage());
+
+    // Fetch parsed data
+    fetchParsedData(file.path, (pageData) => {
+      dispatch(setParsedData({ fileName: file.name, pageData }));
+    }, (reportCard) => {
+      dispatch(setReportCard(reportCard));
+    });
+  };
 
   return (
     <div className="files-page">
@@ -11,7 +30,7 @@ const FilesPage = () => {
       {files.length > 0 ? (
         <ul>
           {files.map((file, index) => (
-            <li key={index}>{file.name}</li>
+            <li key={index} onClick={() => handleFileClick(file)}>{file.name}</li>
           ))}
         </ul>
       ) : (
